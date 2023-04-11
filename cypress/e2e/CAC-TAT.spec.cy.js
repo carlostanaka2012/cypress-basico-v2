4447fe44047c5e90b997754f31765af2e2591d3f
CARLOS HENRIQUE TANAKA <carlostanaka2012@gmail.com>
2023-04-11 11:31:55 -0300

    cadastro e validação mensagem
    cadastro e validacao mensagem

--- cypress/e2e/CAC-TAT.spec.cy.js
+++ cypress/e2e/CAC-TAT.spec.cy.js
@@ -1,10 +1,23 @@
 /// =<reference types="Cypress"  />
 
-describe('Central de Atendimento ao Cliente TAT', () => {
+describe('Central de Atendimento ao Cliente TAT', function() {
+
+  beforeEach(function(){
+    cy.visit('./src/index.html')
+  })
 
   it('verifica o titulo da aplicação', () => {
-    cy.visit('./src/index.html')
-
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+
+  it.only('preencha os campos obrigatorios e envia o formulário', function(){
+    cy.get('#firstName').type('Carlos')
+    cy.get('#lastName').type('Tanaka')
+    cy.get('#email').type('tanaka@testes.com')
+    cy.get('#open-text-area').type('Teste')
+    cy.get('button[type="submit"]').click()
+    
+    cy.get('.success').should('be.visible').contains('Mensagem enviada com sucesso.')
+
   })
 })
